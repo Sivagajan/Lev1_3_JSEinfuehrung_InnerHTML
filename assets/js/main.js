ad12509bfd61e90c10a3bb9ebf329055feb01043
Sivagajan <sivagajan@gmail.com>
2022-07-15 12:02:26 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+console.log('it works')
+
+document.getElementById('gallery').innerHTML ='<figure><img src="https://images.unsplash.com/photo-1606228281437-dc226988dc3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80.jpg" alt="Trulli" style="width:100%"><figcaption>pic 1</figcaption></figure>'
+
+document.getElementById('gallery').innerHTML +='<figure><img src="https://images.unsplash.com/photo-1606228281437-dc226988dc3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80.jpg"style="width:100%"><figcaption>pic 2</figcaption></figure>'
+
+document.getElementById('gallery').innerHTML +='<figure><img src="https://images.unsplash.com/photo-1606228281437-dc226988dc3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80.jpg" style="width:100%"><figcaption>pic 3</figcaption></figure>'
